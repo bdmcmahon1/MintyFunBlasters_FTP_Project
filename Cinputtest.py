9741bde588a39f89567a3bcb8243cea5c43387c7
Charlie Sullivan <csullivan107@gmail.com>
2018-02-20 16:10:24 -0700

    working on the client. started a put test file

---
+++ Cinputtest.py
@@ -0,0 +1,9 @@
+import sys,re,time, select, Header, os
+from socket import *
+
+#create instance of Header cla
+#header = Header()
+
+datatype = raw_input("what would you like to do?")
+#header = Header(datatype)
+print datatype
--- ClientTest1.py
+++ ClientTest1.py
@@ -14,15 +14,14 @@ readData = {}
 def usage():
     print "usage: %s [--serverAddr host:port]" % sys.argv[0]
     sys.exit(1)
-
+#idle state
 def input():
     header = Header()
-    header.datatype = raw_input("What would you like to do:\n 
-	GET - pull file from server \n 
-	PUT - store a file on the server \n")
+    header.datatype = raw_input("What would you like to do:")
     header.options = raw_input("Which file would you like to GET/PUT?")
     header.filesize = os.path.getsize(header.options())
-    
+
+#put request state    
 def putFile(header.options(),msgSize,readData):
     fp = open(header.options(), "rb")
     
@@ -34,9 +33,25 @@ def putFile(header.options(),msgSize,readData):
         index++
         if readData[-1] < msgSize:
             break
-        
+#total time out return to idle        
+def connLost():
+    #code to run when connection to server is lost
 
+    print "Connection to Server lost"
+    #after connection lost revert back to idle and ask for input
+    input()
     
+def getFile():
+    #Send get message to server
+
+    #create file w/ file name from user to store data
+
+    #use select/recieveMsg() to recieve message
+    
+
+
+def recieveMsg():
+    #code that will recieve a message and split the header
 
 try:
     args = sys.argv[1: ]
@@ -73,6 +88,8 @@ writeSock = []# empty list since we arent writing to sockets(used in queue ? )
 errorSock = []# empty list - not interested in errors from select
 timeout = 5# time out variable since select has timeout built in
 
+totalTimeout = 0
+
 #mark start time for RTT calc
 # startTime = time.time()# send the data to the server
 clientSocket.sendto(message, serverAddr)
@@ -83,6 +100,9 @@ readReady, writeReady, errorReady = select.select(readSock, writeSock, errorSock
 # if nothing is present in the sockets print a timeout error
 if not readReady and not writeReady and not errorReady:
     print "timeout: No communication from the server"
+    totalTimeout++
+    if totalTimeout == 5:
+        connLost()
 
 #if there is something present in the read sockets read it and print it out
 for socket in readReady: #dont forget to calculate RTT
