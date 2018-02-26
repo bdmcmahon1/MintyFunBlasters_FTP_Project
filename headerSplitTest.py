cf8672c9ea7a48fa9e5c8cea323175756106786a
Charlie Sullivan <csullivan107@gmail.com>
2018-02-26 13:39:11 -0700

    have fun testing

--- headerSplitTest.py
+++ headerSplitTest.py
@@ -10,9 +10,16 @@ import collections
 #define global variables
 state = 0
 fileDict = {}
+putFile = ""
+#create empty header for use later on
+emptyhdr = Header()
+emptyhdr.Write()
 
+messageSize = 60
 #define constants
 serverAddr =('localhost',10000)
+
+
 
 #define and create client
 clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -24,7 +31,7 @@ clientSocket.setblocking(0)# set blocking to false so timeouts work
 def inputs():
     #define global variables
     global state
-    
+    global putFile
     #create header to populate request message with
     while(1):
         datatype = raw_input("GET or PUT a file?\n")
@@ -37,6 +44,8 @@ def inputs():
         if datatype == "PUT":
             if os.path.isfile(filename):
                 print "Fetching File..."
+                putFile = filename
+                
                 break
             else:
                 print "File does not exist. Choose another file..."
@@ -65,7 +74,7 @@ def inputs():
 def getFile(reqHead):
     #define global variables
     global state
-    fileDict = {}
+    global fileDict = {}
     #send request to server
     clientSocket.sendto(reqHead, serverAddr)
     print "request sent - waiting for response"
@@ -82,7 +91,7 @@ def getFile(reqHead):
             print "message recieved and ready for interpretation"
             
             #parse message and populate the dictionary
-            data,index,fileName = parseMessage(message)
+            data,index,fileName,messageType = parseMessage(message)
             print "data after parse"
             print data
             print "len of data"
@@ -94,6 +103,8 @@ def getFile(reqHead):
             if len(data) == 0:
                 print "File Transfer Complete"
                 saveFile(reqHead)
+                #clear file Dict for next operation
+                fileDict = {}
                 break
             
     #wait for data
@@ -177,32 +188,166 @@ def sockRead():
 #______________________________________________________________________________________________________
 
 def parseMessage(message):
-    hdr = Header()
-    hdr.Write()
-    print"msg"
-    print message
-    headerData = message[:39]
-    print repr(headerData)
-    headerdict = hdr.Read(headerData)
+    #if server message is an empty msg with no header - total timeout
+    if len(message) == 0:
+        return ,0,0,0,0
+    #otherwise handle the file as normal
+    else:
+        hdr = Header()
+        hdr.Write()
+        print"msg"
+        print message
+        headerData = message[:39]
+        print repr(headerData)
+        headerdict = hdr.Read(headerData)
 
     
 
-    #splitMessage = message.split("@")
-    
-    data = message[40:]
-    index = headerdict["sequencenumber"]
-    fileName = headerdict["options"]
-    #index:message populate file dict
-    #if data exists its part of the file and should be stored
-    if len(data) != 0:
+        #splitMessage = message.split("@")
+        messageType = headerdict["datatype"]
+        data = message[40:]
+        index = headerdict["sequencenumber"]
+        fileName = headerdict["options"]
+        #index:message populate file dict
+        #if data exists its part of the file and should be stored
+        if len(data) != 0:
         fileDict[index] = data
+        
+        return data, index, fileName, messageType
+
+
+#______________________________________________________________________________________________________
+#function for put file request
+def putFile(reqHead):
+    #define global variables
+    global state
+    global fileDict = {}
+    global putFile
+    global messageSize
+    global serverAddr
+    global emptyhdr
+    #send request to server
+    messageSend = reqHeader
+    print "header built. Sending PUT request to server..."
+    clientSocket.sendto(messageSend, serverAddr)
+    #wait for ACK regarding request
+    #check and read the socket
+    message=sockRead(messageSend)
+    #if message is recieved split message to hader and data to check ACK
+    data, index, filename, messageType = parseMessage(message)
+    #check for ack
+    if messageType == "ACK":
+        #open file and break it up into fileDict
+        print "Server received request. Preparing file for transport..."
+        #read file and populate dictionary with it
+    else if len(message) == 0:
+        Print "Server Timed out. Closing application..."
+        return
     
-    return data, index, fileName
+    
+    #brians way of doing things---------------------------------
+        
+    #open file and get relevant data out of it for header
+    FILE = open(putFile, "rb")
+    filebytes = len(FILE)
+    filepackets = filebytes/messageSize
+    packetsize = 100
+    FILE.close()
 
+
+    # STOP-AND-WAIT needed here: Send file parts
+    #set up sequence number for sending data
+    seqNumber = 1
+    #for i (start at 0) to end of file (i increments by message size
+    for i in range(0, filebytes+1, msgSize):
+        #Create data header
+        dataHDR = Header.Header()
+        dataHDR.datatype = "DATA"
+        dataHDR.filesize = str(filebytes)
+        dataHDR.numberofpackets = str(filepackets)
+        dataHDR.packetsize = str(packetsize)
+        dataHDR.timetolive = "5"
+        dataHDR.options = putFile
+        dataHDR.sequencenumber = str(seqNumber)
+        dataheader = dataHDR.Write()
+        #match data types just incase using dataheader doesnt work
+        headerBytes = bytearray(dataheader)
+        
+        # Create the message sequence and join with the header
+        #fileOBJ = open(filepath)
+        #open the file > step fid in the file where i is (0 then 60 then 120....)
+        with open(filepath, "rb") as binary_file:
+            #Seek to specified position
+            binary_file.seek(i)
+            #create message with dataheader and reading msgSize bytes from file
+            messageSend = dataheader + binary_file.read(messageSize)
+                        
+            # Send message sequence
+            clientSocketsock.sendto(messageBytes, address)
+                    
+            #STOP-AND-WAIT
+            serverMessage = sockReadPut(messageSend)
+            #check if it is an ACK - parse message first
+            data, index, filename, messageType = parseMessage(message)
+            #if ack is for correct sequence number increment seq num and start the loop over again
+            if messageType == "ACK":
+                seqNumber = seqNumber + 1
+            #if not ack for correct sequence number restart loop at current sequence number
+            #if there was a total timeoutleave this loop - check again in the for loop
+            else if len(serverMessage) == 0:
+                break
+                #file closes automatically with with loop type
+    #if there was a total timeout leave the for loop        
+    if len(serverMessage) == 0:
+           break                  
+    #Send EOF (empty data message with dummt header
+  
+    sentEOF = sock.sendto(emptyhdr.Write() + "", address)
+                
+
+    #-----------------------------------------------------------
 
 #______________________________________________________________________________________________________
 
-#______________________________________________________________________________________________________
+def sockReadPut(messageSend):
+    #define global variables
+    global state
+    global serverAddr
+    print "waiting to hear from server"
+    #for reading msgs from server
+    readSock = [clientSocket]
+    # empty list since we arent writing to sockets(used in queue
+    writeSock = []
+    # empty list - not interested in errors from select
+    errorSock = []
+    # time out variable since select has timeout built in
+    timeout = 1
+
+    totalTimeout = 0
+    
+    #check sockets of server
+    while(1):
+
+        readReady, writeReady, errorReady = select.select(readSock, writeSock, errorSock, timeout)
+        # if nothing is present in the sockets print a timeout error
+        if not readReady and not writeReady and not errorReady:
+            print "timeout: No communication from the server, Resending Packet"
+            #resend message
+            clientSocket.sendto(messageSend, serverAddr)
+            totalTimeout+=1
+            if totalTimeout == 5:
+                state=0  #reset state variable
+                print "connection to server lost"
+                messageRecv = ""
+                return messageRecv
+        else:
+            #something present in the sockets - read it and return it
+            for socket in readReady: #dont forget to calculate RTT
+                messageRecv, serverAddr = socket.recvfrom(2048)
+                print "packet recieved..."
+        
+            return messageRecv
+
 
 #______________________________________________________________________________________________________
 
