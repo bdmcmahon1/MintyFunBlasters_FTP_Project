835bbd720a7f6b9e3c9f85eb22493ffe855402dd
Charlie Sullivan <csullivan107@gmail.com>
2018-03-19 17:24:11 -0600

    does what it says

---
+++ CumulitiveACKtest.py
@@ -0,0 +1,80 @@
+#cumulitive ack testing module
+
+acklist=[]
+ackindex=0
+lastACK=0
+
+while 1:
+	current_ack = int(raw_input("Enter an packet number: "))
+	if current_ack == 99:
+		break
+	
+	if current_ack in acklist:
+		print'duplicat message - ignore'
+		continue
+	
+		
+		
+	
+	
+	acklist.append(current_ack)
+	print acklist
+	if 1 not in acklist:
+		print 'still waiting on first packet in sequence'
+		continue
+		
+	print 'sorting list of acks recieved'
+	sorted_acklist = sorted(acklist)
+	print sorted_acklist
+	
+	
+	print 'checking for missing acks'
+	#test to see if all the acks are there
+	
+	#number of times for loop should iterate if all is good
+	finalCheck = len(sorted_acklist)-lastACK
+	cnt=0
+	if len(sorted_acklist)>1:
+		for i in range(lastACK,len(sorted_acklist)):
+			print 'checking index %d' % i
+			check=sorted_acklist[i]-sorted_acklist[i-1]
+			#print 'check: %d' % check
+			if check > 1:
+				
+				print 'missing a message'
+				bestACK = sorted_acklist[i-1]
+				if bestACK == lastACK:
+					print 'previous ACK sent still best cumulitive ACK'
+					break
+				else:
+					print 'Best cumulitive ack possible: %d' % sorted_acklist[i-1]
+					print 'sending best cumulitive ack'
+					break
+			#ISSUE HERE - IF LAST NUMBER REPEAT IT WONT CHECK THE END OF THE LIST
+			
+				
+			#if last pass through for loop and check was ok	
+			if i == sorted_acklist[-1] and check == 1:
+				print 'no missing packets - best cumulitive ack is latest message'
+				print 'sending latest message ACK'
+			
+			bestACK = sorted_acklist[i]	
+		#if the for loop finishes without missing packets
+			cnt=cnt+1
+			
+			#check if everything went smoothly and it is the final check of the for loop (highest ack receieved)
+
+	
+	else:
+		if acklist[ackindex] == 1:
+			print 'first packet recieved - ACK sent'
+			lastACK=1
+		else:
+			print 'first packet recieved not first packet sent \ndont send ack - Waiting for more packets'
+			lastACK = 1
+			#not sure how to handle the inisial part being 0 - for loop affected
+	
+	if cnt == finalCheck:
+		print 'All messages present - latest message is best ack \n Send ack for message just recieved'
+	ackindex = ackindex+1
+	print 'ready for another message'
